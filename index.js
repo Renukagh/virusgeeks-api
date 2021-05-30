e35be6645fe98fb96ef6bb8f2341bf4c96a849d3
Anji Keesari <anjkeesari@gmail.com>
2021-05-29 20:42:27 -0700

    added get api

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+// require('dotenv').config();
+app.get('/',(req,res)=>{
+    res.send('Response from Node js API');
+})
+// Server Initiate
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
