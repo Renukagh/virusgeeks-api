f3908a6f456e52d10eda8d5bab0759a92f576241
Renukakeesari <Renuka.Keesari@nttdata.com>
2021-05-31 09:57:32 -0700

    added post apis

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-const express = require('express');
-const app = express();
-// require('dotenv').config(); test
-app.get('/',(req,res)=>{
-    res.send('Response from Node js API');
-})
-// Server Initiate
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+/* 
+    this file will have all the application level configuration, as we go ahead we will keep on adding middlewares and supporting libraries
+*/
+
+const express = require('express'); // import express
+const app = express(); // initialise app with express
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+// Logger
+app.use(logger('dev'));
+
+// body parsers
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
+// CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+// import our routes
+const routes = require('./Routes/PostsRoutes');
+// middleware to use our routes
+app.use('/', routes);
+
+// export the app
+module.exports = app;
